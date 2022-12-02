8241c576d77a36d781af778756aad80865083ed2
Tom van den Berg <tom171296@users.noreply.github.com>
2022-12-02 07:10:17 +0000

    update

---
+++ advent-of-code-2022/1Dec/CalorieCounting.cs
@@ -0,0 +1,9 @@
+namespace advent.of.code.first.dec;
+
+public static class CalorieCounting
+{
+    public static int run()
+    {
+
+    }
+}
---
+++ advent-of-code-2022/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var fileData = File.ReadAllLines("1Dec/input.txt");
+
+
+
+var result = Solution.run();
